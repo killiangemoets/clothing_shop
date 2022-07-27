36b9272be80fb284dc2d1b04c45ab3bdbad2dcb7
Killian Gemoets <kilgem@live.fr>
2022-07-27 18:07:01 +0200

    card

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,13 +1,54 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains product to add
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+  // if found increament quantity
+  if (existingCartItem)
+    return cartItems.map((cartItem) => {
+      if (cartItem.id === productToAdd.id)
+        return { ...cartItem, quantity: cartItem.quantity + 1 };
+      else return cartItem;
+    });
+
+  // else add cartItem
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState([]);
+
+  // One way to update the cartCount is by using the useEffect hook.
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, item) => total + item.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
